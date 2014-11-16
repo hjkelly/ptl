753631486bbd6ccc34ee137e5fae0bb0b64db1fa
Harrison Kelly <hjkelly@gmail.com>
2014-11-16 08:33:10 -0500

    Foundation-izing templates

--- ptl/settings/base.py
+++ ptl/settings/base.py
@@ -78,6 +78,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Templates
@@ -85,7 +88,16 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.request',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+)
 
 # django-phonenumber-field
 
